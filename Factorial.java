cc202e78ed7833e5d5abe2657fe40a2540ed24de
GayatriGPagar <gayatripagar@gtasterix.com>
2026-01-07 13:25:29 +0530

    Factoril

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+class Factorial{
+    public static void main(String[] args) {
+        
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("eneter no: ");
+    int no = sc.nextInt();
+
+    int fact = 1;
+    
+    for(int i = 1; i <= no; i++)
+    {
+        fact = fact*i;
+    }
+    System.out.println("the Factorial is :"+fact);
+    }
+}
